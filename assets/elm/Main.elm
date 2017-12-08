c6b092601b572f74265d940e3406323f165b03ee
tamanugi <tamanugi@gmail.com>
2017-12-08 14:35:24 +0900

    add

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,5 +1,54 @@
 module Main exposing (..)
-import Html exposing (text, Html)
+
+import List as L
+
+import Html exposing (text, Html, input, div, button )
+import Html.Attributes exposing (class, id)
+import Html.Events exposing (onClick)
+
+import Phoenix.Socket as Socket exposing (withDebug)
+import Phoenix.Channel as Channel
+import Phoenix.Push as Push
+
+type Msg = Send
+
 main : Html a
 main =
-  text "Hello Foo"
+  div [] [input [id "msg-input"] [], button [onClick Send ] [text "送信"]]
+
+
+init =
+  Socket.init("/socket")
+  |> Socket.withDebug
+
+
+joinChannel payload roomname joinSuccessMsg joinErrorMsg hooks phxMsg model =
+    let
+        channel =
+            Channel.init roomname
+            |> Channel.withPayload payload
+            |> Channel.onJoin joinSuccessMsg
+            |> Channel.onError joinErrorMsg
+        (socket1, cmd) =
+            Socket.join channel model.phxSocket
+        socket2 =
+            L.foldl
+                (\(name, msg) -> Socket.on name roomname msg)
+                socket1
+                hooks
+    in
+    ( { model | phxSocket = socket2 }
+    , Cmd.map phxMsg cmd
+    )
+
+pushSocket msgType topic phxMsg payload model =
+    let
+        push =
+            Push.init msgType topic
+            |> Push.withPayload payload
+        (socket1, socketCmd) =
+            Socket.push push model.phxSocket
+    in
+    ( { model | phxSocket = socket1 }
+    , Cmd.map phxMsg socketCmd
+    )
